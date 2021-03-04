ada59029d2fa4fa01f3a157e17e68c44f9fe6a7e
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-04 18:29:36 +0700

    Update searchInvalidResponse.js

--- player/searchInvalidResponse.js
+++ player/searchInvalidResponse.js
@@ -1,6 +1,32 @@
 module.exports = (client, message, query, tracks, content, collector) => {
     if (content === 'cancel') {
         collector.stop();
-        return message.channel.send(`${client.emotes.success} - The selection has been **cancelled** !`);
-    } else message.channel.send(`${client.emotes.error} - You must send a valid number between **1** and **${tracks.length}** !`);
+        return message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL()
+    },
+    description: "The selection has been **cancelled** !",
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL(),
+      text: "© Hyper"
+    }
+  }
+})
+    } else message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL()
+    },
+    description: `You must send a valid number between **1** and **${tracks.length}** !`,
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL(),
+      text: "© ZerioDev(REMAKE)"
+    }
+  }
+});
 };
