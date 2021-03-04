d1a5595f44125fd05655c05615401cd0afa34aff
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-04 18:34:40 +0700

    Update noResults.js

--- player/noResults.js
+++ player/noResults.js
@@ -1,3 +1,16 @@
 module.exports = (client, message, query) => {
-    message.channel.send(`${client.emotes.error} - No results found on YouTube for ${query} !`);
+    message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL()
+    },
+    description: `${query}`,
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL(),
+      text: "© Hyper"
+    }
+  }
+});
 };
