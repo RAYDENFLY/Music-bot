9c2b357fd5801e4cc04c59ce5292e7d2c431e545
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-04 18:33:27 +0700

    Update playlistAdd.js

--- player/playlistAdd.js
+++ player/playlistAdd.js
@@ -1,3 +1,27 @@
 module.exports = (client, message, queue, playlist) => {
-    message.channel.send(`${client.emotes.music} - ${playlist.title} has been added to the queue (**${playlist.tracks.length}** songs) !`);
+    message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL()
+    },
+    title: "Playlist Add",
+    fields: [
+      {
+      "name": "Playlist Title",
+      "value": `${playlist.title}`,
+      },
+      {
+      "name": "Duration",
+      "value": `${playlist.tracks.length}`,
+      "inline": true
+    }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL(),
+      text: "© Hyper"
+    }
+  }
+})
 };
