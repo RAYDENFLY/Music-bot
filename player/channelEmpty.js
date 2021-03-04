69b9a71c6bd6ffd2fa7fc6035ec0546b628ff924
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-04 18:36:15 +0700

    Update channelEmpty.js

--- player/channelEmpty.js
+++ player/channelEmpty.js
@@ -1,3 +1,16 @@
 module.exports = (client, message, queue) => {
-    message.channel.send(`${client.emotes.error} - Music stopped as there is no more member in the voice channel !`);
+    message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL()
+    },
+    description: `Music stopped as there is no more member in the voice channel !`,
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL(),
+      text: "© ZerioDev (REMAKE)"
+    }
+  }
+});
 };
