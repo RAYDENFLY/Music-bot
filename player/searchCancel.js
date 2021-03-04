0e145abb364a8f550e0690bfe97880bb78e98519
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-04 18:32:08 +0700

    Update searchCancel.js

--- player/searchCancel.js
+++ player/searchCancel.js
@@ -1,3 +1,16 @@
 module.exports = (client, message, query, tracks) => {
-    message.channel.send(`${client.emotes.error} - You did not provide a valid response ... Please send the command again !`);
+    message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL()
+    },
+    description: `${client.emotes.error} - You did not provide a valid response ... Please send the command again !`,
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL(),
+      text: "© ZerioDev(REMAKE)"
+    }
+  }
+});
 };
