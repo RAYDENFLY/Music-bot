9160e3cdd6b87f1b30c0d391f895d599d6c1e377
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2021-03-04 18:32:49 +0700

    Update queueEnd.js

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -1,3 +1,16 @@
 module.exports = (client, message, queue) => {
-    message.channel.send(`${client.emotes.error} - Music stopped as there is no more music in the queue !`);
+    message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL()
+    },
+    description: `Attention! this bot can't run on voice channel 24 hours so please add songs to continue playback`,
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL(),
+      text: "© Hyper"
+    }
+  }
+});
 };
